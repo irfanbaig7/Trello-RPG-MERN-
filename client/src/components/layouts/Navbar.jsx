1ab441e1ef7849bef909935795b6613f360184ba
irfan baig <ibaig9356@gmail.com>
2025-11-29 16:17:36 +0530

    feat: layout set done

---
+++ client/src/components/layouts/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="w-full bg-gray-100 p-4 mb-4 rounded">
+      <h1 className="text-lg font-semibold">Task Game</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import Layout from '../components/layouts/Layout'
 
 const AppRoutes = () => {
-  return (
-    <div>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-        </Routes>
-    </div>
-  )
+    return (
+        <div>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
+            </Layout>
+        </div>
+    )
 }
 
 export default AppRoutes
