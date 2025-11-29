bcaaa74bd258989a15e0fb2e4cbbbc4393ff8e6f
irfan baig <ibaig9356@gmail.com>
2025-11-29 15:53:31 +0530

    basic prj setup an tailwind library installing done

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-red-600">Task Game App</h1>
+    </div>
+  )
+}
+
+export default App
