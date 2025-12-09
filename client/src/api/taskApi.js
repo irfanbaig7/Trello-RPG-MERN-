88b0f70d2710474cd107df715e7863b037b575a3
irfan baig <ibaig9356@gmail.com>
2025-12-09 17:40:45 +0530

    fix: missing return in taskApi axios functions

--- client/src/api/taskApi.js
+++ client/src/api/taskApi.js
@@ -1,13 +1,12 @@
 import { axiosClient } from "./axiosClient";
 
 export const taksApi = {
-    getByProject: (projectId) => {
-        axiosClient.get(`/tasks/project/${projectId}`)
-    },
-    create: (projectId, data) => {
-        axiosClient.post(`/tasks/project/${projectId}`, data)
-    },
-    updateStatus: (taskId, status) => {
-        axiosClient.patch(`/tasks/${taskId}/status`, { status })
-    }
-}
+    getByProject: (projectId) =>
+        axiosClient.get(`/tasks/project/${projectId}`),
+
+    create: (projectId, data) =>
+        axiosClient.post(`/tasks/project/${projectId}`, data),
+
+    updateStatus: (taskId, status) =>
+        axiosClient.patch(`/tasks/${taskId}/status`, { status }),
+};
